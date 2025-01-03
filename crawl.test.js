dfbeb3c678c802a007f949740d2ea89d9788d255
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2025-01-03 17:06:41 +0000

    add tests

---
+++ crawl.test.js
@@ -0,0 +1,17 @@
+const { normalizeURL } = require("./crawl.js")
+const {test, expect} = require("@jest/globals")
+
+test('normalizeURL', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+
+test('normalizeURL', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
